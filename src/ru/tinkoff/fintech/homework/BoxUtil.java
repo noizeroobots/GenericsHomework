3790978c610e7b5468451145bb2053646807bac0
noizeroobots <noizeroobots@gmail.com>
2021-03-12 16:38:23 +0300

    Initial commit

---
+++ src/ru/tinkoff/fintech/homework/BoxUtil.java
@@ -0,0 +1,35 @@
+package ru.tinkoff.fintech.homework;
+
+public class BoxUtil {
+    
+    public class BoxUtil {
+
+        // скопировать содержимое из одной коробки в другую
+        //Box(dest) в которую будем копировать может быть типизирована любым родителем объекта содержащимся в Box(src)
+        public static ... void copyFromBoxToBox(Box<...> src, Box<...> dest) {
+        ...
+        }
+
+        // скопировать содержимое из Box(src) которая может быть типизирована только классом Fruit и его наследниками,
+        // при условии, что содержащийся фрукт свежий (fresh == true).
+        //Box(dest) в которую будем копировать может быть типизирована любым родителем объекта содержащимся в Box(src)
+        public static ... void copyFreshFruitFromBoxToBox(Box<...> src, Box<...> dest) {
+        ...
+        }
+
+        //вывести в консоль (toString()) объект второй коробки
+        public static ... void printElementFromTwoBoxes(Box<...> box) {
+       ...
+        }
+
+        /**
+         * вывести в консоль (toString()) объект последней коробки
+         *
+         * box Box, которая содержит в себе любое кол-во вложенных Box, в последней из которых может быть любой объект.
+         */
+        public static ... void printElementFromBoxes(Box<...> box) {
+        ...
+        }
+    }
+
+}
